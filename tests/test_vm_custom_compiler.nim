e95aff41d1c6aec569d649c4038161a75e0f85ea
Guoliang Cao <gcao99@gmail.com>
2024-01-17 15:16:32 -0500

    Custom compiler

---
+++ tests/test_vm_custom_compiler.nim
@@ -0,0 +1,44 @@
+import unittest, tables
+
+import gene/types
+
+import ./helpers
+
+# Custom compiler
+
+# When a custom compiler is called, code is first compiled then executed automatically
+# Q: How are arguments processed?
+# A:
+# Q: Should compilation output be cached?
+# A:
+# Custom compiler vs function vs macro
+
+# Custom instruction
+
+# Has to be highly efficient
+# Can only be implemented in Nim, e.g.
+# define_instruction "DoX", proc(self: var Frame, inst: Instruction) =
+#   ...
+
+test_vm """
+  (compile c a
+    [
+      ($vm/Push a)
+      ($vm/Push 1)
+      ($vm/Add)
+    ]
+  )
+  (c 1)
+""", 2
+
+test_vm """
+  (compile c a
+    [
+      ($vm/Push a)
+      ($vm/Push 1)
+      ($vm/Add)
+    ]
+  )
+  (var b 1)
+  (c b)
+""", 2
